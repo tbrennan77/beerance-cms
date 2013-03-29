c595fbe3eaecc2b38ece50fe8fb772840a7691dd
Dillon Hafer <dh@dillonhafer.com>
2013-03-29 11:08:02 -0400

    Fixed schema and model for specials to match production schema.

--- app/models/bar_special.rb
+++ app/models/bar_special.rb
@@ -1,4 +1,21 @@
 class BarSpecial < ParseUser
-  fields :bar_id, :bar_location, :bar_name, :special_description, :sale_price, :expiration_prepend, :expiration_date, :beer_color, :is_regular_item
-  validates_presence_of :bar_id, :special_description, :sale_price, :expiration_prepend, :expiration_date, :beer_color, :is_regular_item
+  fields :bar_id, :bar_location, :bar_name, :special_description, :sale_price, :expiration_date, :beer_color
+  validates_presence_of :bar_id, :special_description, :sale_price, :expiration_date, :beer_color
+
+  def set_expiration_date
+    self.expiration_date = ParseDate.new(iso: Time.parse(self.expiration_date).utc.iso8601)
+  end
+
+  def set_geo_location
+    self.bar_location = ParseGeoPoint.new :latitude => 34.09300844216167, :longitude => -118.3780094460731
+  end
+
+  def set_bar_name
+    self.bar_name = BarEntity.find(self.bar_id).bar_name
+  end
+
+  def ensure_formats
+    self.beer_color = self.beer_color.to_f
+    self.sale_price = self.sale_price.to_f
+  end
 end
