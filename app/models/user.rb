6d72fa3c9ebf65dfa323ad262b4dbdfc9c1b6744
Dillon Hafer <dh@dillonhafer.com>
2013-04-03 14:26:29 -0400

    Updated validations and forms for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ParseUser
-  fields :email, :username, :run_count, :owner_name, :owner_phone, :account_type, :expiration_date, :user_favorites, :createdAt, :admin  
+  fields :email, :username, :run_count, :owner_name, :owner_phone, :account_type, :expiration_date, :user_favorites, :createdAt, :admin, :stripe_customer_id, :mon_start, :mon_end 
   validates_presence_of :username, :password, :owner_name, :owner_phone, on: :create
   validates :password, length: {minimum: 6}
+  
   def admin?; self.admin ? true : false; end
   def make_admin; self.admin=true;self.save; end
   def remove_admin; self.admin=false;self.save; end
