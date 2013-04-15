e8d66130e6030145847e6f4e2f837764b7df8737
Dillon Hafer <dh@dillonhafer.com>
2013-04-15 14:28:09 -0400

    Added subscription test and fixed model validation.

--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -1,18 +1,20 @@
 require 'test_helper'
- 
+require File.dirname(__FILE__) + '/../test_helper'
+
 class SubscriptionTest < ActiveSupport::TestCase
   test "it has a subscription plan" do
-    subscription = Subscription.new user_id: 1, expiration_date: Date.today
+    subscription = Subscription.new user_id: "1", expiration_date: Date.today
     assert !subscription.valid?
   end
 
   test "it has a user id" do
-    subscription = Subscription.new expiration_date: Date.today, subscription_plan_id: 1
+    subscription = Subscription.new expiration_date: Date.today, subscription_plan_id: "1"
     assert !subscription.valid?
   end
 
-  test "it has an expiration_date" do
-    subscription = Subscription.new expiration_date: Date.today, user_id: 1
-    assert !subscription.valid?
-  end  
+  test "is sets the expiration date on create" do
+    subscription_plan = SubscriptionPlan.first
+    subscription = Subscription.create user_id: "1", subscription_plan_id: subscription_plan.id
+    assert_equal Date.today.advance(months: subscription_plan.length_in_months), subscription.expiration_date
+  end
 end
