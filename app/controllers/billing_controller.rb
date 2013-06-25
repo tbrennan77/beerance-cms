649f4520b7193bc46a0ca31b41adde52c8f83a34
Dillon Hafer <dh@dillonhafer.com>
2013-06-25 09:52:42 -0400

    Cleaned up billing controller.

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -12,18 +12,18 @@ class BillingController < ApplicationController
   def show_invoice
     @invoice = Stripe::Invoice.retrieve params[:id]
     if @invoice.customer != current_user.stripe_customer_id
-      redirect_to root_path, notice: 'Tricky'
+      redirect_to log_out_path
     end
   end
 
   def update_plan
-    plan = SubscriptionPlan.find(params[:subscription_plan_id])    
+    plan = SubscriptionPlan.find(params[:subscription_plan_id])
     current_user.update_plan(plan) unless current_user.subscription_plan_id == plan.id
     redirect_to billing_overview_path
   end
 
   def update_card
     current_user.update_card(params[:stripe_card_token])
-    redirect_to billing_overview_path, notice: 'Updated Card Info'    
-  end  
+    redirect_to billing_overview_path, notice: 'Updated Card Info'
+  end
 end
