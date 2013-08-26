33b6a012e65b6cd68ed1c5c7cc6e59a0aab485f3
Dillon Hafer <dh@dillonhafer.com>
2013-08-26 15:10:08 -0400

    Fix zip and ip handling.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,13 +11,21 @@ class HomeController < ApplicationController
 
   def map
     params[:distance] = params[:distance] ||= 10
-    zip = params[:zip] || request.remote_ip.to_s
-    geo = Geocoder.search(zip)
-    lat = geo.first.data['geometry']['location']['lat']
-    lng = geo.first.data['geometry']['location']['lng']
+    
+    if params.has_key?(:zip)
+      geo = Geocoder.search(params[:zip])
+      lat = geo.first.data['geometry']['location']['lat']
+      lng = geo.first.data['geometry']['location']['lng']
+      @city = geo.first.data['address_components'][1]['long_name']        
+    else
+      geo = Geocoder.search(request.remote_ip)
+      lat = geo.first.data['latitude']
+      lng = geo.first.data['longitude']
+      @city = geo.first.data['city']
+    end
+    
     @location = [lat, lng]
     
-    @city = geo.first.data['address_components'][1]['long_name']        
     @specials = BarSpecials.near(:bar_location, @location, maxDistanceInMiles: params[:distance].to_i).all
     render layout: 'application'
   end
