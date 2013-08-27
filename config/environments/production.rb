3c4304847a97c1196a4ad0be24fb29060eadb824
Dillon Hafer <dh@dillonhafer.com>
2013-08-27 14:15:40 -0400

    Added static caching.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Beerance::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=2592000"
+  config.action_controller.perform_caching = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
