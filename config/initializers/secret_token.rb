20b125d3fdb90a03bc36b9e83b3a2fed7b440a54
Dillon Hafer <dh@dillonhafer.com>
2013-08-28 18:13:07 -0400

    Removed secret token.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Beerance::Application.config.secret_token = '7c29c4020d9ffa70075d7827cbfd434c45aa633826RRu6t4bvgldusadf352#$%tiiklt3354ihkclkkrindreefasdfdsf1cc69ed138c9a7f495eaaff7219071ae37e0bdc9a2bb66c6352d76af390cc0548218c144ad24aa73c0b79c4d'
