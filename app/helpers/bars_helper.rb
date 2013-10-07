ec6e6d609a61fd79763d57ba5fcf09b7a6b1bd9f
Dillon Hafer <dh@dillonhafer.com>
2013-10-07 14:38:02 -0400

    Updated plan styles

--- app/helpers/bars_helper.rb
+++ app/helpers/bars_helper.rb
@@ -2,4 +2,15 @@ module BarsHelper
   def bar_error(message)
     message.gsub("Latitude can't be blank", "Unable to geocode address").gsub("end", "closed at time").gsub("start", "open at time")
   end
+
+  def class_for_plan(plan)
+    case plan.name
+    when '12_months'
+      'green'
+    when '6_months'
+      'in-brown'
+    when '3_months'
+      'orange'
+    end
+  end
 end
