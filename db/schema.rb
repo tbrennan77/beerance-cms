8c70d5db8adbd1b2d850032f89858a1015a42502
Dillon Hafer <dh@dillonhafer.com>
2013-10-20 22:34:49 -0400

    Updated schema style

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131003164930) do
+ActiveRecord::Schema.define(version: 20131003164930) do
 
-  create_table "bar_specials", :force => true do |t|
+  create_table "bar_specials", force: true do |t|
     t.integer  "bar_id"
     t.integer  "beer_color"
     t.integer  "beer_size"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(:version => 20131003164930) do
     t.decimal  "sale_price"
     t.string   "description"
     t.string   "parse_bar_special_id"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "bars", :force => true do |t|
+  create_table "bars", force: true do |t|
     t.integer  "user_id"
     t.integer  "subscription_plan_id"
     t.string   "stripe_customer_id"
@@ -54,50 +54,50 @@ ActiveRecord::Schema.define(:version => 20131003164930) do
     t.string   "saturday_start"
     t.string   "saturday_end"
     t.string   "parse_bar_id"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "feedbacks", :force => true do |t|
+  create_table "feedbacks", force: true do |t|
     t.integer  "user_id"
     t.string   "category"
     t.text     "comment"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "meta_tags", :force => true do |t|
+  create_table "meta_tags", force: true do |t|
     t.text     "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "news_subscriptions", :force => true do |t|
+  create_table "news_subscriptions", force: true do |t|
     t.string   "promoter_name"
     t.string   "subscriber_email"
     t.string   "subscriber_name"
     t.string   "subscriber_type"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "subscription_plans", :force => true do |t|
+  create_table "subscription_plans", force: true do |t|
     t.integer  "amount"
     t.string   "friendly_name"
     t.string   "image"
     t.integer  "length_in_months"
     t.string   "name"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                   :default => "",    :null => false
-    t.string   "encrypted_password",      :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           :default => 0
+    t.integer  "sign_in_count",           default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -105,12 +105,12 @@ ActiveRecord::Schema.define(:version => 20131003164930) do
     t.boolean  "newsletter_subscription"
     t.string   "name"
     t.string   "phone"
-    t.boolean  "admin",                   :default => false, :null => false
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+    t.boolean  "admin",                   default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
