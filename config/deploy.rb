9561e478fc63e3d269a627ba545e84b6b6be6936
Dillon Hafer <dh@dillonhafer.com>
2013-12-17 15:40:06 -0500

    Moves to www dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "ssh://git@bitbucket.org:boondockwalker/beerance-cms.git"
+set :repository, "ssh://git@bitbucket.org/boondockwalker/beerance-cms.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
